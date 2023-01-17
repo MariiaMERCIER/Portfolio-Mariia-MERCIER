338234206c49567266ae66ccfe748382191ec503
Mariia IVANCHENKO <mariia.mercier@outlook.fr>
2023-01-17 10:06:28 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Mariia MERCIER</p>
+      <nav>
+        <p>Mes projets</p> <p>Qui je suis?</p> <p>Contact</p>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <p>Mariia MERCIER</p>
+      <nav>
+        <p>Mes projets</p> <p>Qui je suis?</p> <p>Contact</p>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
