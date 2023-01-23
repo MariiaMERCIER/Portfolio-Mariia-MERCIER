31338ef2e642b62037160d3b2409af35fef4beb2
Mariia IVANCHENKO <mariia.mercier@outlook.fr>
2023-01-23 14:34:14 +0100

    image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,35 +24,67 @@ const Home = () => {
         </div>
       </div>
       <div className="projects">
-        <img
-          src={require("https://res.cloudinary.com/dcvjq7vzd/image/upload/v1674227186/portfolio/todolist_nz0nvp.png")}
-          alt="todolist"
-        />
+        <a
+          href="https://github.com/MariiaMERCIER/toDoListV3"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src="https://res.cloudinary.com/dcvjq7vzd/image/upload/v1674227186/portfolio/todolist_nz0nvp.png"
+            alt="todolist"
+          />
+        </a>
 
-        <img
-          src={require("https://res.cloudinary.com/dcvjq7vzd/image/upload/v1674227236/portfolio/marvel_zu4xqj.png")}
-          alt="marvel"
-        />
-
-        <img
-          src={require("https://res.cloudinary.com/dcvjq7vzd/image/upload/v1674227231/portfolio/vinted_rtbmqt.png")}
-          alt="vinted"
-        />
-
-        <img
-          src={require("https://res.cloudinary.com/dcvjq7vzd/image/upload/v1674227241/portfolio/netflix_df9elb.png")}
-          alt="netflix"
-        />
-
-        <img
-          src={require("https://res.cloudinary.com/dcvjq7vzd/image/upload/v1674227187/portfolio/deliveroo_s3l5tm.png")}
-          alt="deliveroo"
-        />
-
-        <img
-          src={require("https://res.cloudinary.com/dcvjq7vzd/image/upload/v1674227222/portfolio/tripadvisor_kh3jgn.png")}
-          alt="tripadvisor"
-        />
+        <a
+          href="https://marvel-by-mmercier.netlify.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src="https://res.cloudinary.com/dcvjq7vzd/image/upload/v1674227236/portfolio/marvel_zu4xqj.png"
+            alt="marvel"
+          />
+        </a>
+        <a
+          href="https://vinted-replica-mmercier.netlify.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src="https://res.cloudinary.com/dcvjq7vzd/image/upload/v1674227231/portfolio/vinted_rtbmqt.png"
+            alt="vinted"
+          />
+        </a>
+        <a
+          href="https://my-netflix-movies.netlify.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src="https://res.cloudinary.com/dcvjq7vzd/image/upload/v1674227241/portfolio/netflix_df9elb.png"
+            alt="netflix"
+          />
+        </a>
+        <a
+          href="https://my-deliveroo-replique.netlify.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src="https://res.cloudinary.com/dcvjq7vzd/image/upload/v1674227187/portfolio/deliveroo_s3l5tm.png"
+            alt="deliveroo"
+          />
+        </a>
+        <a
+          href="https://tripadvisor-mariiamercier.netlify.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src="https://res.cloudinary.com/dcvjq7vzd/image/upload/v1674227222/portfolio/tripadvisor_kh3jgn.png"
+            alt="tripadvisor"
+          />
+        </a>
       </div>
     </main>
   );
